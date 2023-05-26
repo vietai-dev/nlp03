97b7d68829195ac3ea7d92f9c0a1081ec48b5f51
pvm <manh2604.pham@gmail.com>
2023-05-26 13:40:17 +0700

    fix train_gpt

--- week6/distributed/example/train_gpt.py
+++ week6/distributed/example/train_gpt.py
@@ -11,9 +11,6 @@ $ torchrun torchrun --standalone --nproc_per_node=2 train_gpt.py
 To run with DDP on 4 gpus across 2 nodes, example:
 - Run on the first (master) node with example IP 123.456.123.456:
 $ torchrun --nproc_per_node=2 --nnodes=2 --node_rank=0 --master_addr=123.456.123.456 --master_port=1234 train_gpt.py
-- Run on the worker node:
-$ torchrun --nproc_per_node=2 --nnodes=2 --node_rank=1 --master_addr=123.456.123.456 --master_port=1234 train_gpt.py
-(If your cluster does not have Infiniband interconnect prepend NCCL_IB_DISABLE=1)
 """
 
 import os
