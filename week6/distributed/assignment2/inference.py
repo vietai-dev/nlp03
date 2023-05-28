895a3ad3f906c93fd26bc718c7780979c345d68c
Manh Pham <70097274+Phammanh26@users.noreply.github.com>
2023-05-28 20:54:31 +0700

    Update inference.py

--- week6/distributed/assignment2/inference.py
+++ week6/distributed/assignment2/inference.py
@@ -22,7 +22,6 @@ def generate_inference(instruction: str, user_inp: str, model_path:str, lora_wei
     top_k=40
     top_p=128
     temperature=0.1
-    lora_weights_path=3
     load_8bit=True
     num_beams =1
     max_new_tokens =128
